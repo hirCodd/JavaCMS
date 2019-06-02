6fc0d2f7d3dc7b620838670dfeb4dd6b05777529
hirCodd <hefengen@hotmail.com>
2019-06-02 14:19:21 +0800

    init

---
+++ src/main/java/top/finen/cms/javacms/JavacmsApplication.java
@@ -0,0 +1,13 @@
+package top.finen.cms.javacms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacmsApplication.class, args);
+    }
+
+}
